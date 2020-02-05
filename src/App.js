e982c38e3c8671d3fd15390913b1337e6eb52171
Daniel Shamany <dan@thinkpad-p1-rhel.lan>
2020-02-05 11:20:22 -0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import ExampleComponent from './components/ExampleComponent';
 import NavBar from './components/NavBar';
+import ExampleComponent from './components/ExampleComponent';
+import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <NavBar navItems={['home', 'about']} />
-      <Switch>
-        <Route exact path="/" component={ExampleComponent} />
-        <Route exact path="/about" render={() => {
-          return(
-            <div>
-              <h1>About</h1>
-            </div>
-          )
-        }} />
-      </Switch>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBar navItems={['home', 'about']} />
+			<Switch>
+				<Route exact path="/" component={ExampleComponent} />
+				<Route exact path="/about" render={() => {
+					return (
+						<div>
+							<h1>About</h1>
+						</div>
+					)
+				}} />
+				<Route exact path="/sign" component={Login} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
