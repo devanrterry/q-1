d07cff29f554a40a5a7b368312ea38630174be0a
devan <devanrterry@gmail.com>
2020-03-02 16:50:48 -0800

    created card list component

---
+++ src/components/CardList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CardContainer.css';
+import CardContainer from './CardContainer'
+
+export default function CardContainer(props){
+    return (
+        <div className='container'>
+            <CardContainer/>
+    
+        </div>
+    )
+}
